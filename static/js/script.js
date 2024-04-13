ae665e3d03b0fbe0c5a5ce3a5ec2f09120a91d6b
Ankithats <ankithats2@gmail.com>
2024-04-13 11:18:45 +0530

    file

---
+++ static/js/script.js
@@ -0,0 +1,15 @@
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+    var header = document.getElementById("header");
+    if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+        header.classList.add("scrolled");
+    } else {
+        header.classList.remove("scrolled");
+    }
+}
+
+
+
+
+
