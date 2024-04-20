a28f0e1640afe642a099cd99ed10effa39f3f7bb
Ankithats <ankithats2@gmail.com>
2024-04-20 11:58:27 +0530

    file

--- demoapp/urls.py
+++ demoapp/urls.py
@@ -3,4 +3,11 @@ from . import views
 app_name='app'
 urlpatterns = [
     path('',views.home,name='home'),
+    path('contact/',views.contact,name='contact'),
+    path('test/',views.test,name='test'),
+    path('about/',views.about,name='about'),
+    path('branch/',views.branch,name='branch'),
+    path('package/',views.package,name='package'),
+    path('blog/',views.blog,name='blog'),
+    path('gal/',views.gal,name='gal'),
 ]
--- demoapp/views.py
+++ demoapp/views.py
@@ -4,3 +4,19 @@ from django.shortcuts import render
 # Create your views here.
 def home(req):
     return render(req,'index.html')
+def contact(req):
+    return render(req,'contact.html')
+def test(req):
+    return render(req,'test.html')
+def about(req):
+    return render(req,'about.html')
+def branch(req):
+    return render(req,'branch.html')
+def package(req):
+    return render(req,'package.html')
+def blog(req):
+    return render(req,'blog.html')
+
+def gal(req):
+    return render(req,'gal.html')
+
